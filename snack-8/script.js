bad43e873d6b9d07adc3051f4844733f312dfc50
MicheleCasani <casanimichele@gmail.com>
2025-04-02 14:43:14 +0200

    [UPD] snack-8 done

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const marcoClass = students.find ((element)=>{
+  return element.class === '3C'
+});
+
+console.log(marcoClass.class)
